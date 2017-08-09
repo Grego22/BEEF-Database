9d1ee9500b5f29e8b4c687b4cd6ab07967748b0d
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-09 10:59:51 -0400

    testing

--- db.init.js
+++ db.init.js
@@ -6,6 +6,18 @@ const databse = pgPromise({databse:'lecture'})
 database.none('DROP TABLE pets')
   .then(()=>{
     console.log('successfully dropped the table 'pets'')
+    //create a TABLE
+    databse.none('CREATE TABLE "pets"(
+                "id" SERIAL PRIMARY KEY
+                "name" VARCHAR (100)NOT NULL
+                "furcolor" VARCHAR (100) NULL
+                "adoptedDate" TIMESTAMP
+                "breed" VARCHAR(100) NOT NULL
+            ').then()( => {
+              console.log('successfully created table "pets"')
+
+
+            })
   })
   .catch((error)=>{
     console.log(error, 'nope')
