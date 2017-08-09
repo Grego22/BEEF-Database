ce88e4faa4eefd752625e5164b0e7a2da4622edf
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-09 10:12:19 -0400

    dropping table

--- db.init.js
+++ db.init.js
@@ -1,3 +1,12 @@
 const pgPromise = require('pg-promise')
 
 const databse = pgPromise({databse:'lecture'})
+
+//DROP the TABLE
+database.none('DROP TABLE pets')
+  .then(()=>{
+    console.log('successfully dropped the table 'pets'')
+  })
+  .catch((error)=>{
+    console.log(error, 'nope')
+  })
